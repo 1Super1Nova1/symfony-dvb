99643bd169ee6861db7fe170ec7c24b3211e2d41
VovaDanilchyk <bvdanilchyk@gmail.com>
2025-03-22 11:46:34 +0200

    Лабораторна 2

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -2,9 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TextController;
+use App\Http\Controllers\ControlerProduct;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/TextController', [TextController::class, 'test']);
+Route::get('/TextController', [
+    TextController::class,
+    'test'
+]);
+
+
+Route::get('/api/v1/products', [
+    ControlerProduct::class,
+    'getProducts'
+]);
+
+Route::get('/api/v1/products/{id}', [
+    ControlerProduct::class,
+    'getProductItem'
+]);
+
+Route::post('/api/v1/products', [
+    ControlerProduct::class,
+    'createProduct'
+])->withoutMiddleware([VerifyCsrfToken::class]);
+
+Route::delete('/api/v1/products/{id}', [
+    ControlerProduct::class,
+    'deleteProduct'
+])->withoutMiddleware([VerifyCsrfToken::class]);
